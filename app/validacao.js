56885df05558632fff5d30de93818e94155160b4
ispadilha <israel.padilha@gmail.com>
2023-09-22 07:12:00 -0300

    Created reload button and function

--- app/validacao.js
+++ app/validacao.js
@@ -15,6 +15,8 @@ function verificaSeOChutePossuiUmValorValido(chute){
         document.body.innerHTML = `
             <h2>Você acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
+
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
         `
     } else if (numero > numeroSecreto) {
         elementoChute.innerHTML += `
@@ -25,7 +27,6 @@ function verificaSeOChutePossuiUmValorValido(chute){
             <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
         `
     }
-
 }
 
 function chuteForInvalido(numero){
@@ -35,3 +36,9 @@ function chuteForInvalido(numero){
 function numeroForMaiorOuMenorQueOValorPermitido(numero){
     return numero > maiorValor || numero < menorValor
 }
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente'){
+        window.location.reload()
+    }
+})
